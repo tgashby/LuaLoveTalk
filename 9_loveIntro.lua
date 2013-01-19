0e44c16e423345369d57766622c7d28e22ec3c08
Tag Ashby <tgashby@calpoly.edu>
2013-01-19 12:06:50 -0800

    Cleaned it up and added a small slide

---
+++ 9_loveIntro.lua
@@ -0,0 +1,5 @@
+--[[
+The idea and greatness behind Love is that it takes care of the game loop and events for you.
+There are pre-defined functions that you use to make your game without having to worry about the low level
+stuff.
+]]
